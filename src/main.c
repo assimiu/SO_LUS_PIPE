fe1baf79ff5a9e91acdd73522d8cc4bd796cfdac
Assimiu Mané <assimium2@gmail.com>
2023-10-09 00:54:43 +0100

    a

--- src/main.c
+++ src/main.c
@@ -4,50 +4,44 @@
 #include<stdlib.h>
 #include<fcntl.h>
 #include<sys/stat.h>
+#include<string.h>
 #define BUFFER_SIZE 42
-#define FIFO_PATH "fifo1"
 
 void criarFifo(char *str)
 {
     if (mkfifo(str, 0666) == -1)
         printf("Erro na criação de pipe\n");
     else
-        printf("FIFO %s criado com sucesso\n", str);
+        printf("FIFO %s criado com sucesso!\n", str);
 }
 
 int roud10(char *str)
 {
-    int i = 0;
-    while (str[i])
-        i ++;
-    if (i < 7)
-        return 1;
-    
     return str[6] == '1' && str[7] == '0'? 0 : 1;
 }
-void ler(char *nameFile)
+void ler(char *name_r, char *name_w, int i)
 {
-    int size;
     int fd;
+    int fd_w;
     char buffer[BUFFER_SIZE];
+    char str_tem[BUFFER_SIZE];
 
-    fd = open(nameFile, O_RDONLY);
+    fd_w = open(name_w, O_WRONLY);
+    fd = open(name_r, O_RDONLY);
+    
     do
     {
-        size = read(fd, buffer, BUFFER_SIZE - 1);
+        read(fd, buffer, BUFFER_SIZE - 1);
         buffer[40] = '\0';
-        buffer[41] = '\n';
-        write(STDIN_FILENO, buffer, size);
-    }while (roud10(buffer));
-    close(fd);
-}
 
-void escrever(char *nameFile, char *msg)
-{
-    int fd;
-    fd = open(nameFile, O_WRONLY);
-    write(fd, msg, sizeof(msg));
+        if (strcmp(buffer, str_tem))
+            printf("Msg: %s\n", buffer);
+        strcpy(buffer, str_tem);
+
+    } while (roud10(buffer));
+    printf("Corrida terminou para carro numero %i\n", i);
     close(fd);
+    close(fd_w);
 }
 
 
@@ -57,7 +51,7 @@ int main(int argc, char *argv[])
     // abrir o FIFO para leitura
     //…
     // ler mensagem do FIFO
-    char comando[100];
+    char comando[36];
     int input;
     char carroIn[50];
     char carroOut[50];
@@ -67,48 +61,13 @@ int main(int argc, char *argv[])
     sprintf(carroIn, "CARRO%i.in", input);
     sprintf(carroOut, "CARRO%i.out", input);
 
-    criarFifo(carroIn);
     criarFifo(carroOut);
+    criarFifo(carroIn);
     if (argc >= 2)
     {
         sprintf (comando, "/bin/bash ./testscript.sh %s &", argv[1]);
         system(comando);
     }
-    escrever(carroOut, "");
-    ler(carroIn);
+    ler(carroIn, carroOut, input);
 
 }
-
-
-/*
-int main(int argc, char **argv)
-{
-    int input;
-    char carroIn[50];
-    char carroOut[50];
-    char buffer[41];
-    if (argc >= 2)
-    {
-        printf("Qual o numero do carro? %s\n", argv[1]);
-        scanf("%i", &input);
-        sprintf(carroIn, "CARRO%i.in", input);
-        sprintf(carroOut, "CARRO%i.out", input);
-
-
-        criarFifo(carroOut);
-        criarFifo(carroIn);
-        printf("Fim da criacao\n");
-
-        printf("inicio excucao\n");
-        
-        printf("leitura\n");
-        escrever(carroIn, "");
-        ler(carroOut, buffer);
-    }
-    else
-    {
-        printf("Numero de argumentos invalido");
-    }
-    return 0;
-}
-*/
